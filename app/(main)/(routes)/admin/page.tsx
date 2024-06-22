6fe36aefa7f633d55f4b71b11611704e2e828875
ttqteo <tuquangtran123@gmail.com>
2024-06-22 16:09:24 +0700

    feat: setup schema and api

--- app/(main)/(routes)/admin/page.tsx
+++ app/(main)/(routes)/admin/page.tsx
@@ -2,14 +2,31 @@
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircleIcon } from "lucide-react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const AdminPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Untitled",
+    });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <h2 className="text-lg font-medium">Welcome to {user?.firstName}</h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircleIcon className="w-4 h-4 mr-2" />
         Create a note
       </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import { ConvexClientProvider } from "@/components/provider/convex-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="theme"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
