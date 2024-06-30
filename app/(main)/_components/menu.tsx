ce02c5b321df22e463af3c34b858e83d15c26989
ttqteo <tuquangtran123@gmail.com>
2024-07-01 00:04:42 +0700

    banner component

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontalIcon, TrashIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface MenuProps {
+  documentId: Id<"documents">;
+}
+export const Menu = ({ documentId }: MenuProps) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note.",
+    });
+    router.push("/admin");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size={"sm"} variant={"ghost"}>
+          <MoreHorizontalIcon className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        alignOffset={8}
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive}>
+          <TrashIcon className="w-4 h-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="text-xs text-muted-foreground p-2">
+          Last edited by: {user?.firstName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className="h-10 w-10" />;
+};
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,8 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 import { Title } from "./title";
 
 interface NavbarProps {
@@ -20,8 +22,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -44,8 +49,12 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
